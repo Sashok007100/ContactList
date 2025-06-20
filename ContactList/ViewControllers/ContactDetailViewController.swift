e2b17b74c607cc5f90281c9775a0ffc5969c62aa
Artemov Aleksandr <sasha.artemov.68@mail.ru>
2025-06-20 19:39:59 +0300

    Был добавлен ViewController для отображения детальной информации о контакте. Также экран был связан с классом ContactDetailViewController.

---
+++ ContactList/ViewControllers/ContactDetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ContactDetailViewController.swift
+//  ContactList
+//
+//  Created by Alexandr Artemov on 20.06.2025.
+//
+
+import UIKit
+
+class ContactDetailViewController: UIViewController {
+
+    @IBOutlet var phoneLabel: UILabel!
+    @IBOutlet var emailLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}

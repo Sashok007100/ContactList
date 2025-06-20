ffe73f00f8da246cdea425ccf39dd963ef9b0857
Artemov Aleksandr <sasha.artemov.68@mail.ru>
2025-06-20 19:15:56 +0300

    Были переопределены методы для указания количества строк и наполнение каждой ячейки этой строки.

--- ContactList/ViewControllers/ContactsTableViewController.swift
+++ ContactList/ViewControllers/ContactsTableViewController.swift
@@ -17,3 +17,20 @@ class ContactsTableViewController: UITableViewController {
     }
 }
 
+// MARK: - UITableViewDataSource
+extension ContactsTableViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        mock.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "contactCell", for: indexPath)
+        
+        var config = cell.defaultContentConfiguration()
+        config.text = mock[indexPath.row].fullName
+        
+        cell.contentConfiguration = config
+        return cell
+    }
+}
+

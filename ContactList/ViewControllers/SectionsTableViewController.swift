59d22208e20603f1eb54e299c5e1e62e6fd52634
Artemov Aleksandr <sasha.artemov.68@mail.ru>
2025-06-21 12:52:16 +0300

    Было настроено количество строк в каждой секции. Был настроен заголовок для каждой секции. Была настроена ячейка, чтобы записи соответствовали контакту, указанному в названии секции.

--- ContactList/ViewControllers/SectionsTableViewController.swift
+++ ContactList/ViewControllers/SectionsTableViewController.swift
@@ -22,7 +22,27 @@ final class SectionsTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        mock[section].rows.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        mock[section].fullName
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "contactDetailCell", for: indexPath)
+        var config = cell.defaultContentConfiguration()
         
-        return 0
+        
+        if indexPath.row == 0 {
+            config.image = UIImage(systemName: "phone")
+            config.text = mock[indexPath.section].rows[indexPath.row]
+        } else {
+            config.image = UIImage(systemName: "tray")
+            config.text = mock[indexPath.section].rows[indexPath.row]
+        }
+        
+        cell.contentConfiguration = config
+        return cell
     }
 }
